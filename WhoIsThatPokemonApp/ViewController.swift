3769d9f8ec61faf332ec6c9729d54d95369df7e9
Luis Fernando Sánchez Palma <luisfer.san.palm@gmail.com>
2024-03-04 12:25:25 -0600

    Inicio de proyecto

---
+++ WhoIsThatPokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoIsThatPokemonApp
+//
+//  Created by Luis Fernando Sánchez Palma on 04/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
